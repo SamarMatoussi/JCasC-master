581787d34ca639241d7934bf898d3ba4f1bc5396
SamarMatoussi <148507480+SamarMatoussi@users.noreply.github.com>
2024-05-21 15:27:15 +0100

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -2,32 +2,14 @@ pipelineJob('pipeline') {
   definition {
     cps {
       script(
-'''pipeline {
-    agent {
-        docker {
-            image 'pipeline/jenkins\'
-            args '-u 0:0 -v /var/run/docker.sock:/var/run/docker.sock\'
-        }
-    }
+              '''pipeline {
+    agent any
+    
     stages {
-        stage('git') {
+        stage('Display Maven Version') {
             steps {
-                echo 'cloning the repository\'
-                // git branch: 'main', credentialsId: 'github-credentials', url: 'https://repositoryURL\'
-                sh 'docker run hello-world\'
-            }
-        }
-        stage('unit_test') {
-            steps {
-                sh 'echo "start the unit test"\'
-            }
-        }
-        stage('build') {
-            steps {
-                sh 'echo "login to the GitHub container registry"\'
-                // Supprimez le bloc withCredentials actuel et utilisez plutôt votre identifiant GitHub et un jeton d'accès personnel pour vous connecter
-                sh 'docker login docker.pkg.github.com -u $GITHUB_USERNAME -p $GITHUB_TOKEN\'
-                sh 'echo "building and pushing images to the container registry"\'
+                // Affichage de la version de Maven
+                sh 'mvn --version'
             }
         }
     }
