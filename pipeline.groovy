e6e8960adaf38448ae1e7d6ed4707d23fe73327e
SamarMatoussi <148507480+SamarMatoussi@users.noreply.github.com>
2024-05-22 10:10:48 +0100

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,20 +1,26 @@
 pipelineJob('pipeline') {
-  definition {
-    cps {
-      script(
-              '''pipeline {
-    agent any
-    
-    stages {
-        stage('Display Maven Version') {
-            steps {
-                // Affichage de la version de Maven
-                sh 'mvn --version'
-            }
+    definition {
+        cps {
+            script(
+                    '''
+                pipeline {
+                    agent {
+                        docker {
+                            image 'pipeline/jenkins'
+                            args '-u 0:0 -v /var/run/docker.sock:/var/run/docker.sock'
+                        }
+                    }
+                    stages {
+                        stage('Display Maven Version') {
+                            steps {
+                                // Affichage de la version de Maven
+                                sh 'mvn --version'
+                            }
+                        }
+                    }
+                }
+                '''
+            )
         }
     }
-}
-''')
-    }
-  }
 }
