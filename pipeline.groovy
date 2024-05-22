297456a4605975533ca6a6fe6a79821ca4e71cc0
SamarMatoussi <148507480+SamarMatoussi@users.noreply.github.com>
2024-05-22 10:45:53 +0100

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,26 +1,38 @@
 pipelineJob('pipeline') {
-    definition {
-        cps {
-            script(
-                    '''
-                pipeline {
-                    agent {
-                        docker {
-                            image 'pipeline/jenkins'
-                            args '-u 0:0 -v /var/run/docker.sock:/var/run/docker.sock'
-                        }
-                    }
-                    stages {
-                        stage('Display Maven Version') {
-                            steps {
-                                // Affichage de la version de Maven
-                                sh 'mvn --version'
-                            }
-                        }
-                    }
-                }
-                '''
-            )
+  definition {
+    cps {
+      script(
+'''pipeline{
+    agent{
+            docker { image 'pipeline/jenkins'
+                        args '-u 0:0 -v /var/run/docker.sock:/var/run/docker.sock'}}
+    stages{
+        stage('git'){
+            steps{
+                echo 'cloning the repository'
+                /*git branch: 'main', credentialsId: 'github-credentials', url: 'https://repositoryURL' */
+                sh 'docker run hello-world'
+            }
+        }
+        stage('unit_test'){
+            steps{
+                sh 'echo "start the unit test"'
+            }
+        }
+        stage('build'){
+            steps{
+                sh 'echo "login to the github container registry"'
+                withCredentials([usernamePassword(credentialsId: 'github-credentials', passwordVariable: 'PASSWORD', usernameVariable: 'USER')]) {
+                    sh 'whoami'
+                    sh 'echo "${USER}"'
+                    sh 'echo "${PASSWORD}"'
+                    sh 'docker login --username "${USER}" --password "${PASSWORD}" registry.github.com'
+            }
+                    sh 'echo "building and pushing images to the container registry"'
+            }
         }
     }
+}''')
+    }
+  }
 }
